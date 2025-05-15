37788288039ac6bd1d9673884d8e414847f94430
Yousef Sayed <uchiha.joee@gmail.com>
2025-05-16 02:11:59 +0300

    Update server.py

--- server.py
+++ server.py
@@ -335,5 +335,4 @@ def download_file(filename):
         return jsonify({'error': f'Error downloading file: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
